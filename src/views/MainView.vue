ff8b56ccf4bd7e7c012d9972c5786e38f938b6a5
yoonju1030 <allrealjgt@gmail.com>
2023-08-17 21:02:51 +0900

    feat: Install vuetify

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,19 +1,10 @@
 <template>
     <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
-      test
+        This is Sakura Tunes
     </div>
   </template>
   
   <script>
   // @ is an alias to /src
-  import HelloWorld from '@/components/HelloWorld.vue'
-  
-  export default {
-    name: 'HomeView',
-    components: {
-      HelloWorld
-    }
-  }
+
   </script>
